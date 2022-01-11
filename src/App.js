941624ae5c5224c49edd6e540ee7a907a14c9a23
Aaron Byrom <aaronbyrom@protonmail.com>
2022-01-11 19:37:47 +1100

    add api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+let apiKey = process.env.API_KEY;
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hey there, what a cool app! 
+          apiKey: {apiKey}
         </p>
         <a
           className="App-link"
