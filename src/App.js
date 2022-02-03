a3a49261e1d12a9f79b41e95ad92eccfe0073a92
Aaron Byrom <aaronbyrom@protonmail.com>
2022-02-03 14:19:06 +1100

    add stuff

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import { useState, useEffect } from 'react';
 import MouseParticles from 'react-mouse-particles'
 
-const fetchData = async (url) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
 
 function App() { 
   const [value, setValue] = useState({
